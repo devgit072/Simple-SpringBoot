05692e1c99397c33da427e09ff909c4695c910f6
Devraj Singh <devrajvit@gmail.com>
2019-09-03 22:32:03 +0530

    Added swagger UI for docs

---
+++ src/main/java/com/devraj/tutorials/simpleSpringBoot/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package com.devraj.tutorials.simpleSpringBoot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+// Access the UI with url: http://localhost:8080/swagger-ui.html
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.devraj.tutorials.simpleSpringBoot.Controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(getApiInfo()
+        );
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder().title("spring-boot apis")
+                .description("Spring boot tutorials")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0")
+                .build();
+    }
+}
